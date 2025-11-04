e311a05cc4ef710279a9c642d3c946df837cc48d
shubhamlodha21 <shubhamlodha2111@gmail.com>
2025-11-04 12:12:28 +0530

    pattern

---
+++ Patterns/2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    for (int i=0;i<5;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ Patterns/c7.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n=5;
+
+    //ROWS
+    for (int i=0;i<n;i++) 
+    { 
+        //Space
+        for (int j=0;j<n-i-1;j++) 
+        { 
+            cout<<" ";
+        }
+        
+        //Stars
+        for (int j=0;j<2*i+1;j++) 
+        { 
+            cout<<"*";
+        }
+
+        //Space
+        for (int j=0;j<n-i-1;j++) 
+        { 
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
